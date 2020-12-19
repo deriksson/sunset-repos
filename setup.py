5a910d98e1d543e7650bd3c37fe18a0c3082424b
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-19 11:28:14 +0100

    Add package description

--- setup.py
+++ setup.py
@@ -2,14 +2,28 @@
 
 """
 
-# Third-party imports
 from setuptools import setup, find_packages
+
+
+with open("README.adoc", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
 
 
 setup(
     name='sunset-repos',
     version='1.0.0',
     author='Daniel Eriksson',
+    author_email="gustaf.daniel.eriksson@gmail.com",
+    description="Archive batches of GitHub repositories",
+    long_description=long_description,
+    long_description_content_type="text/asciidoc",
+    url="https://github.com/deriksson/sunset-repos",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
