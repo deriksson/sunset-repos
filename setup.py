4b86a778e42477b0ac86f3fb4ed31a8b7c24c47b
deriksson <gustaf.daniel.eriksson@gmail.com>
2020-12-19 02:45:05 +0100

    [release] Bump major version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sunset-repos',
-    version='0.0.1',
+    version='1.0.0',
     author='Daniel Eriksson',
     packages=find_packages(),
     include_package_data=True,
